5fc12441ed08b572dafc0520fffe2949fd0f2518
patel_utsav_1318 <patelutsav312@gmail.com>
2026-06-20 10:10:48 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,21 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   esbuild: {
+//     loader: 'jsx',
+//     include: /src\/.*\.js$/,
+//     exclude: []
+//   },
+//   server: {
+//     port: 3000,
+//     host: true,
+//     open: true
+//   }
+// });
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
 export default defineConfig({
   plugins: [react()],
   esbuild: {
@@ -12,5 +27,8 @@ export default defineConfig({
     port: 3000,
     host: true,
     open: true
+  },
+  build: {
+    outDir: 'build'
   }
 });
